9f6618ab7d064182cf935aad586b7dfc4bdedc62
margaretbole1100 <margaretbole1100@gmail.com>
2023-06-15 19:54:53 -0500

    refactored code

--- day-5.py
+++ day-5.py
@@ -14,33 +14,24 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 #initialize variables
-letters_list_len = 0
-letters_len = len(letters) - 1
+
 password_list = []
-
-numbers_list_len = 0
-numbers_len = len(numbers) -1
-
-symbols_list_len = 0
-symbols_len = len(symbols) - 1
 
 password = ''
 
-for letters_list_len in range(0,nr_letters):
-    password_list.append(letters[random.randint(0, letters_len)])
-    letters_list_len += 1
+for letters_list_len in range(0, nr_letters):
+    password_list.append(random.choice(letters))
 
 for numbers_list_len in range(0, nr_numbers):
-    password_list.append(numbers[random.randint(0, numbers_len)])
+    password_list.append(random.choice(numbers))
 
 for symbols_list_len in range(0, nr_symbols):
-    password_list.append(symbols[random.randint(0, symbols_len)])
+    password_list.append(random.choice(symbols))
 
 for element in password_list:
     password += str(element)
 
-print(password)
-
+print(f'Easy password is {password}')
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
@@ -50,6 +41,7 @@ random_order_list_len = 0
 random_order_list = []
 password_list_len = len(password_list) 
 
+# A list shuffle could be used
 while random_order_list_len < password_list_len:
     rand_nr = random.randint(0, password_list_len-1)
     if rand_nr not in random_order_list:
@@ -61,7 +53,6 @@ hard_password = ''
 for number in random_order_list:
     hard_password += str(password_list[number])
 
-print(hard_password)
-
+print(f'Hard password is {hard_password}')
 
 
