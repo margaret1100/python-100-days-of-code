53e4a9dccf8f99b36c1dc899fb1200dc3618cbe6
margaretbole1100 <margaretbole1100@gmail.com>
2023-06-20 18:56:05 -0500

    Added lives and hangman stage

--- day-7.py
+++ day-7.py
@@ -2,6 +2,65 @@
 
 import random
 
+stages = [
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+'''
+]
+
 word_list = ["aardvark", "baboon", "camel"]
 
 # pick a random word and assign it to chosen word
@@ -11,18 +70,28 @@ word_length = len(chosen_word)
 # Create list to put letter display
 display = []
 for n in range(word_length):
-  display.append("_")
+    display.append("_")
 
-blanks_left = display.count('_')
+lives = 6
+end_of_game = False
 
-while blanks_left > 0:
-  guess = input("Pick a letter\n").lower()
-  
-  # check for match and replace matches with the guess
-  for position in range(word_length):
-    if guess == chosen_word[position]:
-      display[position] = guess
-  blanks_left = display.count('_')
-  print(display)  
-  
+while end_of_game == False:
+    guess = input("Pick a letter\n").lower()
 
+    if guess in chosen_word and guess not in display:
+        # check for match and replace matches with the guess
+        for position in range(word_length):
+            if guess == chosen_word[position]:
+                display[position] = guess
+        blanks_left = display.count('_')
+        print(display)
+        # Check if word has been solved.
+        if '_' not in display:
+            end_of_game = True
+            print('You won!')
+    else:
+        lives -= 1
+        print(stages[lives])
+        if lives == 0:
+            end_of_game = True
+            print('You lose!')
