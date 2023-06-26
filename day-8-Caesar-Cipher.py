183797e0561052bfae855789fd28a82842149373
margaretbole1100 <margaretbole1100@gmail.com>
2023-06-25 21:37:36 -0500

    Completed program with while loop

--- day-8-Caesar-Cipher.py
+++ day-8-Caesar-Cipher.py
@@ -1,27 +1,36 @@
+import day_8_caesar_cipher
+
 alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(day_8_caesar_cipher.logo)
+continue_program = 'yes'
 
-
-# Encoding function - encrypting the message
 def caesar(given_text, shift_amount, cipher_direction):
-  returned_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  # Handle non-alphabetic code 
-  for letter in given_text:
-      if letter not in alphabet:
-        returned_text += letter
-      else:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        returned_text += alphabet[new_position]
-  return (returned_text)
+    shift_amount %= 26
+    returned_text = ""
+    if cipher_direction == "decode":
+      shift_amount *= -1
+    # Handle non-alphabetic code 
+    for letter in given_text:
+        if letter not in alphabet:
+          returned_text += letter
+        else:
+          position = alphabet.index(letter)
+          new_position = position + shift_amount
+          returned_text += alphabet[new_position]
+    return (returned_text)
 
-print(caesar(text, shift, direction))
+
+while continue_program == 'yes':
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  # Encoding function - encrypting the message
+  print(caesar(text, shift, direction))
+  continue_program = input("If you would like to continue type 'yes'")
+
+print("Goodbye")
