8fd66a603a2414599677a09f1ef72d4b1aebda25
margaretbole1100 <margaretbole1100@gmail.com>
2023-07-01 14:45:34 -0500

    Added looping functionality

--- day_10_calculator.py
+++ day_10_calculator.py
@@ -32,3 +32,24 @@ operation = input("Pick an operation from the line above.")
 answer = function_dict[operation](num1, num2)
 
 print(f"{num1} {operation} {num2} = {answer}")
+
+keep_calculating = True
+
+while keep_calculating == True:
+	calculating_decision = input("Would you like to continue calculating type y or n?")
+	if calculating_decision.lower() == 'n':
+		keep_calculating = False
+		break
+
+	for function in function_dict:
+		print(function)
+
+	operation = input("Pick an operation from the line above.")
+	num3 = int(input("What is the next number?"))
+	next_answer = function_dict[operation](answer, num3)
+	
+	print(f"{answer} {operation} {num3} = {next_answer}")
+
+	answer = next_answer
+
+
