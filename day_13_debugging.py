7ae486f936e007c29382c36b20b44fdadda7b5bf
margaret <margaretbole1100@gmail.com>
2023-07-09 16:50:22 -0500

    Fixed FizzBuzz code bugs

--- day_13_debugging.py
+++ day_13_debugging.py
@@ -70,11 +70,11 @@ else:
 
 # FizzBuzz Code
 for number in range(1, 101):
-  if number % 3 == 0 or number % 5 == 0:
+  if number % 3 == 0 and number % 5 == 0: #changed or to and operator
     print("FizzBuzz")
-  if number % 3 == 0:
+  elif number % 3 == 0: # changed if statements to elif
     print("Fizz")
-  if number % 5 == 0:
+  elif number % 5 == 0:
     print("Buzz")
   else:
-    print([number])
+    print(number) # changed to just print number, not array number
