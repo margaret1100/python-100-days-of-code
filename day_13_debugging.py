18eee682478446ffce4f11a6d4569f3f1943df81
margaret <margaretbole1100@gmail.com>
2023-07-09 16:39:57 -0500

    Added fixed odd-even

--- day_13_debugging.py
+++ day_13_debugging.py
@@ -47,7 +47,7 @@ mutate([1,2,3,5,8,13])
 # Odd Even
 number = int(input("Which number do you want to check?"))
 
-if number % 2 = 0:
+if number % 2 == 0: # Fixed to add extra = sign for comparison
   print("This is an even number.")
 else:
   print("This is an odd number.")
