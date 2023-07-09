1af148fcfb0612a7ca7afbff924d8e679f166cf8
margaret <margaretbole1100@gmail.com>
2023-07-09 16:41:17 -0500

    Added leap year coe with bug

--- day_13_debugging.py
+++ day_13_debugging.py
@@ -53,3 +53,16 @@ else:
   print("This is an odd number.")
   
 
+#Leap year code
+year = input("Which year do you want to check?")
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
