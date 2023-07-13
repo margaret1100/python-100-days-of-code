3f9503f04bf4f413656f00e116d3e40c13a83bfa
margaret <margaretbole1100@gmail.com>
2023-07-12 23:20:54 -0500

    Simplified Code

--- day_14_higher_lower_game.py
+++ day_14_higher_lower_game.py
@@ -1,15 +1,15 @@
 
+##### Higher Lower Game #####
+
 import day_14_higher_lower_game_art
 from os import system
 from day_14_higher_lower_game_data import data
-from random import randint
+import random
 
 
 def get_account():
 
-	number_of_accounts = len(data)
-	account_number = randint(0, number_of_accounts - 1)
-	account = data[account_number]
+	account = random.choice(data)
 	return account
 
 
@@ -19,28 +19,23 @@ def compare_accounts(account_a, account_b, answer):
 	print(day_14_higher_lower_game_art.logo)
 
 	if account_a["follower_count"] > account_b["follower_count"]:
-		if answer == "a":
-			return "correct"
-		else:
-			return "incorrrect"
+		return answer == "a"
 	else:
-		if answer == "b":
-			return "correct"
-		else:
-			return "incorrrect"
+		return answer == "b"
 
-
+# format the account information to print
 def print_accounts(account):
 	return (f"{account['name']}, {account['description']}, from {account['country']}")
 
 
-
 def play_game():
+	
 	print(day_14_higher_lower_game_art.logo)
 	score = 0
 
 	my_account_a = get_account() 
 
+	# Once the user is wrong the game ends via a return
 	while True:
 		my_account_b = get_account()
 
@@ -56,7 +51,7 @@ def play_game():
 
 		my_answer = input("who has more followers? Type 'A' or 'B':").lower()
 		
-		if compare_accounts(my_account_a, my_account_b, my_answer) == "correct":
+		if compare_accounts(my_account_a, my_account_b, my_answer) == True:
 			my_account_a = my_account_b
 			score += 1
 			print(f"You're Right! Current score: {score}")
@@ -64,9 +59,6 @@ def play_game():
 		else:
 			print(f"Sorry, that's wrong: Final score: {score}")
 			return 
-
-
-
 
 
 play_game()
